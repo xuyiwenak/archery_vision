e3abc7fa9e8bb66bd50342dbf01551b316d64878
han0 <wslshanlin@sina.com>
2025-03-20 08:51:09 +0800

    feat: 新增枚举类型 ActionState

---
+++ src/enums/action_state.py
@@ -0,0 +1,10 @@
+from enum import Enum, unique
+
+@unique
+class ActionState(Enum):
+    """射箭动作状态枚举类"""
+    LIFT = "Lift"       # 举弓
+    DRAW = "Draw"       # 开弓
+    SOLID = "Solid"     # 固势
+    RELEASE = "Release" # 撒放
+    UNKNOWN = ""        # 未知状态
