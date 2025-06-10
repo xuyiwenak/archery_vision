4aaa77a6c4a25cb2263b2673ced50bd88ec3aa1d
ivan <Ivan@dinoverse.games>
2025-06-10 16:28:07 +0800

    增加默认模型名称

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     output_dir = os.path.join('data', 'output')
     os.makedirs(input_dir, exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
-
+    default_model_name = 'yolo11x-pose'
     # 处理输入目录中的所有视频文件
     for filename in os.listdir(input_dir):
         if filename.lower().endswith(('.mp4', '.avi', '.mov', '.mkv')):
             input_path = os.path.join(input_dir, filename)
             output_path = os.path.join(output_dir, f'output_{filename}')
-            YoloBow.process_video(input_path, output_path)
+            YoloBow.process_video(input_path, output_path, default_model_name)
 
 if __name__ == "__main__":
     main()
