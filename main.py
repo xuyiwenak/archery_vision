1b6e5dfa8e20606a8af0aaaff8cf498c52501a59
ivan <Ivan@dinoverse.games>
2025-06-10 16:04:10 +0800

    处理文件后缀大小写

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from src.models.yolo_bow import YoloBow
-
+from src.core.log import logger
 def main():
     # 确保输入和输出目录存在
     input_dir = os.path.join('data', 'input')
@@ -10,7 +10,7 @@ def main():
 
     # 处理输入目录中的所有视频文件
     for filename in os.listdir(input_dir):
-        if filename.endswith(('.mp4', '.avi', '.mov', '.mkv')):
+        if filename.lower().endswith(('.mp4', '.avi', '.mov', '.mkv')):
             input_path = os.path.join(input_dir, filename)
             output_path = os.path.join(output_dir, f'output_{filename}')
             YoloBow.process_video(input_path, output_path)
